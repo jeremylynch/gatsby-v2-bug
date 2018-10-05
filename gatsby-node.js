67cfc9afb7d40b81f349d086f3d14430d2d95c69
Jeremy Lynch <jeremylynch1994@gmail.com>
2018-10-05 13:05:07 +1000

    initial commit

---
+++ gatsby-node.js
@@ -0,0 +1,17 @@
+const path = require(`path`);
+
+exports.createPages = ({actions}) => {
+  const templatePath = path.resolve('src/pages/page-2.js')
+
+  const pages = ["new", "used", "demo"]
+
+  pages.map((type) => (
+    actions.createPage({
+      path: `${type}-cars`,
+      component: templatePath,
+      context: {
+        car_type: 'test',
+      },
+    })
+  ))
+};
